f17097a46652e2158a3d95b7fd13d94d0e72c2b2
samuelliyi <liyisepku@gmail.com>
2017-01-15 01:00:02 +0800

    fix typo

--- R/eGraph.R
+++ R/eGraph.R
@@ -1,26 +1,26 @@
 eGraph = function(nodes, edges, title=NULL, tooltip="",size=c(100,100),opt = list())
 {	
-	data = lapply(split(nodes,seq_len(nrow(nodes))),as.list)
-	link = lapply(split(edges,seq_len(nrow(edges))),as.list)
-	names(data) = NULL
-	names(link) = NULL
-	opt$series = list(
-		list(
-			type = "graph",
-			data = list(data),
-			link = list(link)
-		)
-
-	)
-	opt$size = size
-	### output list format
-	chart = htmlwidgets::createWidget(
-		'echarts', opt,
-		package = 'recharts', width = size[1], height = size[2],
-		preRenderHook = function(instance) {
-			instance
-		}
-	)
-	chart = .addClass(chart, "eGraph")
-	chart
+  data = lapply(split(nodes,seq_len(nrow(nodes))),as.list)
+  link = lapply(split(edges,seq_len(nrow(edges))),as.list)
+  names(data)=NULL
+  names(link)=NULL
+  opt$series = 
+    list(
+      type = "graph",
+      data = data,
+      links = link
+    )
+  
+  
+  opt$size = size
+  ### output list format
+  chart = htmlwidgets::createWidget(
+    'echarts', opt,
+    package = 'recharts', width = size[1], height = size[2],
+    preRenderHook = function(instance) {
+      instance
+    }
+  )
+  chart = .addClass(chart, "eGraph")
+  chart
 }
